043e7e9ac82a660fae25f44ffee09c667cdfcd1e
143NoName <shabaikovich.andrew.2004@gmail.com>
2025-10-26 13:05:41 +0300

    Initial Commit

---
+++ ScheduleBSUIR/ScheduleBSUIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleBSUIRApp.swift
+//  ScheduleBSUIR
+//
+//  Created by user on 26.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleBSUIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
